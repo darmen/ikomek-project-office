3df14c154a22716a1f1e70872139d05bc3d24031
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2018-07-18 22:37:37 +0600

    Adapt localization in routes

--- src/routes/ikomek/project-office.php
+++ src/routes/ikomek/project-office.php
@@ -1,14 +1,16 @@
 <?php
 
-Route::group(
-[
-    'prefix' => 'project-office',
-    'namespace' => 'Darmen\IKomekProjectOffice\app\Http\Controllers',
-], function () {
+Route::group(['prefix' => LaravelLocalization::setLocale()], function(){
+    Route::group(
+        [
+            'prefix' => 'project-office',
+            'namespace' => 'Darmen\IKomekProjectOffice\app\Http\Controllers',
+        ], function () {
 
-    Route::get('/', 'ProjectOfficeController@index')->name('ikomek.project-office');
-    Route::get('/category/{category_id}', 'ProjectOfficeController@category')->name('ikomek.project-office.office');
-    Route::get('/category/{category_id}', 'ProjectOfficeController@category')->name('ikomek.project-office.category');
-    Route::get('/project/{project_id}', 'ProjectOfficeController@project')->name('ikomek.project-office.project');
+        Route::get('/', 'ProjectOfficeController@index')->name('ikomek.project-office');
+        Route::get('/category/{category_id}', 'ProjectOfficeController@category')->name('ikomek.project-office.office');
+        Route::get('/category/{category_id}', 'ProjectOfficeController@category')->name('ikomek.project-office.category');
+        Route::get('/project/{project_id}', 'ProjectOfficeController@project')->name('ikomek.project-office.project');
 
+    });
 });
